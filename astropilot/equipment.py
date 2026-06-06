16da0920f4f481f3257a035c68b4c5f01f4e23aa
Franck Perruchoud <fp74osteo@gmail.com>
2026-06-06 20:12:51 +0200

    Version initiale AstroPilot

---
+++ astropilot/equipment.py
@@ -0,0 +1,41 @@
+EQUIPMENT_PROFILES = {
+    "seestar_s30": {
+        "name": "Seestar S30",
+        "focal_length_mm": 150,
+        "aperture_mm": 30,
+        "sensor_width_mm": 5.6,
+        "sensor_height_mm": 3.2,
+    },
+
+    "seestar_s50": {
+        "name": "Seestar S50",
+        "focal_length_mm": 250,
+        "aperture_mm": 50,
+        "sensor_width_mm": 5.6,
+        "sensor_height_mm": 3.2,
+    },
+
+    "widefield_135mm": {
+        "name": "APS-C + 135 mm",
+        "focal_length_mm": 135,
+        "aperture_mm": 50,
+        "sensor_width_mm": 22.3,
+        "sensor_height_mm": 14.9,
+    },
+
+    "newton_150_750": {
+        "name": "Newton 150/750 APS-C",
+        "focal_length_mm": 750,
+        "aperture_mm": 150,
+        "sensor_width_mm": 22.3,
+        "sensor_height_mm": 14.9,
+    },
+
+    "newton_200_1000": {
+        "name": "Newton 200/1000 APS-C",
+        "focal_length_mm": 1000,
+        "aperture_mm": 200,
+        "sensor_width_mm": 22.3,
+        "sensor_height_mm": 14.9,
+    },
+}
---
+++ astropilot/targets.py
@@ -0,0 +1,43 @@
+TARGET_OBJECTS = {
+    "M31": {
+        "ra": 10.6847,
+        "dec": 41.2692,
+        "size_arcmin": 140,
+    },
+
+    "M42": {
+        "ra": 83.8221,
+        "dec": -5.3911,
+        "size_arcmin": 85,
+    },
+
+    "M51": {
+        "ra": 202.4842,
+        "dec": 47.2306,
+        "size_arcmin": 11,
+    },
+
+    "M81": {
+        "ra": 148.8882,
+        "dec": 69.0653,
+        "size_arcmin": 27,
+    },
+
+    "M101": {
+        "ra": 210.8023,
+        "dec": 54.3489,
+        "size_arcmin": 28,
+    },
+
+    "Rosette": {
+        "ra": 97.5,
+        "dec": 4.95,
+        "size_arcmin": 80,
+    },
+
+    "NorthAmerica": {
+        "ra": 314.75,
+        "dec": 44.33,
+        "size_arcmin": 120,
+    },
+}
