1fc222757d10266cf3403569c8bc3a378f6ef1d4
Franck Perruchoud <fp74osteo@gmail.com>
2026-06-27 21:26:14 +0200

    Refactor: move best_windows into SkyEngine

--- astropilot/engines/sky_engine.py
+++ astropilot/engines/sky_engine.py
@@ -1,8 +1,10 @@
 import math
+from datetime import timedelta
 from astral import moon
 from astral.moon import moonrise, moonset
 from astral.sun import sun
 from astral import moon
+from astropilot.user_profile import load_user_profile
 from astropy.coordinates import SkyCoord, get_body, EarthLocation, AltAz
 from astropy.time import Time
 import astropy.units as u
@@ -464,6 +466,243 @@ class SkyEngine:
             "sqm": sqm,
                 
             }
+    
+    
+    def best_windows(self,hours, moon_illumination, moon_rise, moon_set, observer, lat, lon,bortle=4, target="deep_sky", target_object="M31",target_obj=None, goal="balanced", window_size= None, limit= 3):
+
+        if target_obj is None:
+            raise ValueError("target_obj is required")
+        
+        profile = load_user_profile()
+
+        if window_size is None:
+            window_size = profile.get("preferences", {}).get("window_size", 2)
+
+        sky = self
+
+        windows = self.iter_windows(hours, window_size)
+
+        if not windows:
+            return []
+
+        candidates = []
+
+        for window in windows:
+
+            visible = sky.moon_visible_during_window(
+                window[0]["time"],
+                window[-1]["time"] + timedelta(hours=1),
+                moon_rise,
+                moon_set
+            )
+
+            scores = []
+            hour_details = []
+            moon_impacts = []
+            moon_penalties = []
+
+            profile = load_user_profile()
+
+            min_alt = profile.get("preference",{}).get("min_altitude_deg",30)
+
+            for h in window:
+                #target_obj = TARGET_OBJECTS[target_object]
+
+                geometry = self.hour_geometry(
+                    h,
+                    observer,
+                    target_obj,
+                    lat,
+                    lon
+                )
+
+                moon_elevation = geometry["moon_elevation"]
+                target_alt = geometry["target_altitude"]
+                moon_sep = geometry["moon_target_sep"]
+                if target_alt < min_alt:
+                    continue
+
+                result = self.score_hour(
+                    h,
+                    moon_illumination,
+                    observer,
+                    lat,
+                    lon,
+                    target_obj,
+                    bortle,
+                    target,
+                    goal,
+                )
+
+                obj_meta = target_obj or {}
+
+                difficulty = obj_meta.get("difficulty", 2)
+                magnitude = obj_meta.get("magnitude", 8)
+                obj_type = obj_meta.get("type", "unknown")
+
+                object_bonus = 0
+
+
+                target_bonus = 0
+
+                if goal == "nebulae" and obj_type == "nebula":
+                    target_bonus += 12
+
+                elif goal == "galaxies" and obj_type == "galaxy":
+                    target_bonus += 12
+
+                ######elif goal == "clusters" and obj_type == "cluster":
+                    #####target_bonus += 12
+                
+                ####from astropilot.equipment_profiles import (
+                    ###CURRENT_EQUIPMENT,
+                    ##get_fov
+                #)
+            
+                frame_bonus = 0
+
+                object_size = obj_meta.get("size_arcmin", 30) / 60
+                ratio = 1
+
+                ####object_size = obj_meta.get("size_arcmin", 30) / 60
+                ###frame_width = fov["width_deg"]
+
+                ##frame_diag = (fov["width_deg"]**2 + fov["height_deg"]**2) ** 0.5
+                #ratio = object_size / frame_diag
+
+
+                preference_bonus = 0
+
+                if goal == "galaxies" and obj_type == "galaxy":
+                    preference_bonus = 25
+
+                elif goal == "nebulae" and obj_type in ["nebula", "planetary_nebula"]:
+                    preference_bonus = 25
+
+                elif goal == "widefield" and object_size >= 1.0:
+                    preference_bonus = 8
+
+                elif goal == "small_targets" and object_size <= 0.5:
+                    preference_bonus = 8
+
+                if obj_type in ["planetary_nebula"]:
+                    ideal_min = 0.02
+                    ideal_max = 0.20
+                elif obj_type in ["galaxy"]:
+                    ideal_min = 0.05
+                    ideal_max = 0.40
+                elif obj_type in ["cluster"]:
+                    ideal_min = 0.10
+                    ideal_max = 0.60
+                else:  # nebula
+                    ideal_min = 0.25
+                    ideal_max = 0.45
+
+                if ideal_min <= ratio <= ideal_max:
+                    object_bonus += 20
+                elif ratio < ideal_min / 2:
+                    object_bonus -= 25
+                elif ratio < ideal_min:
+                    object_bonus -= 10
+                elif ratio > ideal_max * 1.5:
+                    object_bonus -= 35
+                elif ratio > ideal_max:
+                    object_bonus -= 15            
+
+                # Bonus difficulté : objets faciles favorisés
+                if difficulty == 1:
+                    object_bonus += 4
+                elif difficulty == 2:
+                    object_bonus += 2
+                elif difficulty >= 4:
+                    object_bonus -= 4
+
+                # Bonus magnitude : objets brillants favorisés
+                if magnitude <= 4:
+                    object_bonus += 4
+                elif magnitude <= 7:
+                    object_bonus += 2
+                elif magnitude >= 9:
+                    object_bonus -= 3
+
+                # Bonus type selon la lune
+                if obj_type == "galaxy" and moon_illumination > 50:
+                    object_bonus -= 5
+                elif obj_type in ["nebula", "planetary_nebula"] and moon_illumination > 50:
+                    object_bonus -= 2
+                elif obj_type == "cluster" and moon_illumination > 50:
+                    object_bonus += 2
+                    
+
+                result["score"] = max(0, min(100, result["score"] + object_bonus + preference_bonus))
+
+                scores.append(result["score"])
+                hour_details.append(result["details"])
+                moon_impacts.append(result["moon_impact"])
+                moon_penalties.append(result["moon_penalty"])
+
+            if not scores:
+                continue
+
+            avg = round(sum(scores) / len(scores))
+
+            avg_alt = sum(
+                d["target_altitude"] for d in hour_details
+            ) / len(hour_details)
+
+            if avg_alt < 20:
+                avg -= 20
+            elif avg_alt < 30:
+                avg -= 10
+
+            moon_avg = round(
+                sum(moon.elevation(observer, h["time"]) for h in window) / len(window),
+                1
+            )
+
+            candidates.append({
+                    "start": window[0]["time"],
+                    "end": window[-1]["time"] + timedelta(hours=1),
+                    "score": avg,
+                    "hour_scores": scores,
+                    "details": hour_details,
+                    "clouds": round(
+                        sum(
+                            h["cloud_cover_low"] * 0.2 +
+                            h["cloud_cover_mid"] * 0.3 +
+                            h["cloud_cover_high"] * 0.5
+                            for h in window
+                        ) / len(window)
+                    ),
+                    "humidity": round(
+                        sum(h["relative_humidity_2m"] for h in window) / len(window)
+                    ),
+                    "wind": round(
+                        sum(h["wind_speed_10m"] for h in window) / len(window),
+                        1
+                    ),
+                    "moon_impact": moon_impacts[0],
+                    "moon_penalty": round(
+                        sum(moon_penalties) / len(moon_penalties),
+                        1
+                    ),
+                    "moon_elevation": moon_avg,
+                    "moon_sep": round(
+                        sum(d["moon_sep"] for d in hour_details) / len(hour_details),
+                        1
+                    ),
+                    "target_altitude": round(
+                        sum(d["target_altitude"] for d in hour_details) / len(hour_details),
+                        1
+                    ),
+                    "sqm": round(
+                        sum(d["sqm"] for d in hour_details) / len(hour_details),
+                        2
+                    ),
+                })
+        return sorted(candidates, key=lambda x: x["score"], reverse=True)[:limit]
+
+                
     def iter_windows(self, hours, window_size):
         if len(hours) < window_size:
             return []
@@ -474,4 +713,5 @@ class SkyEngine:
             windows.append(hours[i:i + window_size])
 
         return windows
-                
+    
+    
