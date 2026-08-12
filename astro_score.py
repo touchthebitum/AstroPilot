91e37e3580477c0520d1613cc9b665fe58e0526c
Franck Perruchoud <fp74osteo@gmail.com>
2026-08-12 21:17:42 +0200

    cleanup: remove unnecessary weather global

--- astro_score.py
+++ astro_score.py
@@ -2703,7 +2703,6 @@ portfolio_engine = PortfolioEngine(
 
 def main(argv=None) -> int:
     global nights
-    global weather
 
     CURRENT_EQUIPMENT = get_active_equipment()
     print("\nSetup actif :", CURRENT_EQUIPMENT)
