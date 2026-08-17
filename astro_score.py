318c5b7d9e77f061b8b3b9b57c75af2cedc9bda6
Franck Perruchoud <fp74osteo@gmail.com>
2026-08-17 16:04:28 +0200

    cleanup: remove duplicate season windows

--- astro_score.py
+++ astro_score.py
@@ -115,24 +115,7 @@ value.get("width_arcmin", 30)),
     for key, value in CATALOG.items()
     if "ra" in value and "dec" in value
 }
-SEASON_WINDOWS = {
-    "M31": {
-        "best_months": [8, 9, 10, 11],
-        "ok_months": [7, 12],
-    },
-    "IC1396": {
-        "best_months": [6, 7, 8, 9],
-        "ok_months": [5, 10],
-    },
-    "Rosette": {
-        "best_months": [12, 1, 2, 3],
-        "ok_months": [11, 4],
-    },
-        "Sh2-129": {
-        "best_months": [7, 8, 9],
-        "ok_months": [6, 10],
-    },
-}
+
 OBJECT_SIZES = {
     "M31": 140,
     "M42": 85,
